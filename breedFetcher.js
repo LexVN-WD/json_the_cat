09b72d91f3252183ae62b5e89d22fc3f71ae1040
Alexander <apvannug.lhl.webdev@gmail.com>
2022-11-02 14:54:12 -0600

    refactor to watch for edge cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,25 @@
-const breed = process.argv[2];
 const request = require('request');
 
-const fetchBreed = function(breed) {
-  let breedURL = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
+const breed = process.argv[2];
+// set breed command argument position
+
+const breedFetcher = function(breed) {
+  let breedURL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+  // create variable for concatenating URL with command argument value
   request(breedURL, (error, response, body) => {
-    console.log(body);
-    console.log(typeof body);
     const data = JSON.parse(body);
+    // parse string and return the value
     if (data.length === 0) {
       console.log("This breed cannot be found");
+      // if the length of data is 0, there is nothing in the object therefore there is no data
+    } else if (JSON.parse(error) !== null) {
+      console.log(`"message": "404 - please consult the documentation for correct url to call. https://docs.thecatapi.com/"`)
+      // of the parsed value of an error is not null (ie if there is a error response), console log the error on their website
     } else {
       console.log(data[0].description);
+      // else print the description of the breed
     }
   });
 };
 
-fetchBreed(breed);
+breedFetcher(breed);
