06560514baefa9dd34b2e8e8166eea2e154727b1
Alexander <apvannug.lhl.webdev@gmail.com>
2022-11-02 14:25:29 -0600

    create function that lets dynamically search breed description by name

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,18 @@
+const breed = process.argv[2];
 const request = require('request');
-const breedName = process.argv[2];
 
-request(breedURL, (error, response, body) => {
-  // console.log(body);
-  // console.log(typeof body);
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  console.log(data[0].description);
-});
+const fetchBreed = function(breed) {
+  let breedURL = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
+  request(breedURL, (error, response, body) => {
+    console.log(body);
+    console.log(typeof body);
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log("This breed cannot be found");
+    } else {
+      console.log(data[0].description);
+    }
+  });
+};
 
-// const fetchBreed = function(breed, callback) {
-//   request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-
-//   })
-// }
+fetchBreed(breed);
